844661e284438bd97b6493e98c75ad5a28bcc3ea
liuchzzyy <liuchzzyy@gmail.com>
2026-02-10 12:47:42 +0800

    Fix config exports

--- src/zotero_mcp/utils/config/__init__.py
+++ src/zotero_mcp/utils/config/__init__.py
@@ -4,6 +4,7 @@ from .config import (
     _clear_cache,
     get_config,
     get_config_path,
+    get_relevant_env_prefixes,
     load_config,
 )
 from .logging import get_logger, log_task_end, log_task_start
@@ -13,6 +14,7 @@ __all__ = [
     "get_config",
     "load_config",
     "get_config_path",
+    "get_relevant_env_prefixes",
     "get_logger",
     "log_task_end",
     "log_task_start",
